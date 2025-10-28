85bcc161df539bb522f26c0decf9b215f4abf2b9
john thornton <jb087011@gmail.com>
2025-10-28 14:10:04 -0400

    Add API documentation

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
 import taskRoutes from './routes/taskRoutes.js';
 
 const app = express();
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+
+const specs = YAML.load('./public/bundled.yaml');
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
